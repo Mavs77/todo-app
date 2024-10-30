ddebb31ad9a7a659e1416a293cd0f76ac5f95923
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-10-29 22:17:02 -0500

    adding src files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// each tab needs to be represented with separate module 
+
+//"new project" tab likely works with a form to create a new object which will be imported into the "view projects" tab
+//"View Projects" tab will just display the list of projects and tasks along with 
+//"Add Task" Allows you to add sub task to a project you select
